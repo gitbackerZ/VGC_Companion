edf93e371438748fe136efd242b16e7cd916da82
gitbackerZ <rodiyuju57@gmail.com>
2026-08-16 10:51:47 +0800

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,10 +22,18 @@ subprojects {
 
 subprojects {
     val configureJvm = {
+        (project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension)?.apply {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+
         tasks.withType<JavaCompile>().configureEach {
             sourceCompatibility = "17"
             targetCompatibility = "17"
         }
+
         tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
             compilerOptions {
                 jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
