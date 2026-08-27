082bf63aeee47148f5e890328f4ac8e8f4e02a64
gitbackerZ <rodiyuju57@gmail.com>
2026-08-27 21:04:01 +0800

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -29,6 +29,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
+        maven { url = uri("https://www.jitpack.io") }
     }
 }
 
